ee67ab06799f0781398374ac1d59f9320b15746f
Brian Cole <Brian Cole>
2016-07-27 11:36:29 -0400

    update 7_27_16

--- Auction.py
+++ Auction.py
@@ -5,24 +5,41 @@ passMe = []
 
 
 def auctionSetup():
+    #Initializing a variable that holds the item that'll eventually be auctioned
     auctionItem = raw_input("What would you like to put up for auction? ")
+    
+    #Initializing a variable that holds the reserved for the item
     itemPrice = raw_input("What is "+auctionItem+" reserved Price? $")
+    
+    #This try & except statement is used to make sure the user entered a number. If they didn't, it starts over.
     try:
         float(itemPrice)
         itemPrice = float(itemPrice)
     except:
         print "Please enter only numbers"
         auctionSetup()
+
+    #Adds the item to be auctioned and it's price to a dictionary. Item name being the key, and the price the value    
     forAuction[auctionItem] = itemPrice
+
+    #Checks if another item will entered in the auction
     decision = raw_input("Do you want to add another item? (enter y/n)").lower()
     if decision == "y":
         auctionSetup()
     if decision == "n":
+        print "T
         participantSetup()
+    else:
+        print "Please enter 'y' or 'n'"
+        decision = raw_input("Do you want to add another item? (enter y/n)").lower()
+        if decision == "y":
+            auctionSetup()
+        if decision == "n":
+            participantSetup()
 
 
 def participantSetup():
-    
+    #A loop that collects all the participants, placing them in a list
     while True :
         print "***Type 'done' when there are no other participants***"
         participant = raw_input("If you plan to participate in the auction, Please enter your first and last name or an auction name(Username): ")
@@ -30,17 +47,18 @@ def participantSetup():
             break
         if len(participant) < 3:
             print "Must be atleast 3 characters long..Try again"
+            participantSetup()
         else:
             participants.append(participant)
         
-        
+    #Goes into the dictionary of auctionable items, and selects the first one (which will still be random). Then sets it to a variable    
     itemBeingAuct = forAuction.keys()[0]
     print itemBeingAuct
-    print "Alright, We will now start the auction off with a " + itemBeingAuct
+    print "Alright, We will now start the auction for the " + itemBeingAuct
     Auction(itemBeingAuct)
 
 def Auction(itemBeingAuct):
-      
+    #Gets the starting bid. Then makes sure it isn't higher than the reserved price.  
     highestBid = raw_input("What is the starting bid? $")
     try:
         float(highestBid)
@@ -56,9 +74,10 @@ def Auction(itemBeingAuct):
     elif highestBid < float(forAuction.get(itemBeingAuct)):
         print "The starting bid is $"+str(highestBid)
 
- 
+     #Just explaining how to pass if you can't bid any higher
     print "***If you cannot bid higher than the Highest Bid at any point, just enter '0' and you will be skipped***"  
-    
+
+    #A loop that basically gets bids. If you skip yourself by enter "0", your name is entered into a list. It does this until everyone skips except the highest bidder of course.
     while True:
         for person in participants:
             bid = raw_input(person + ",please enter your bid amount: $")
@@ -94,13 +113,21 @@ def Auction(itemBeingAuct):
 
         if len(passMe) == len(participants)-1:
             break
-                
+
+    #Letting it be known the auction is over. Also letting us know who the winner is & whether it as success or not.         
     print"The bidding for the "+itemBeingAuct+" is now over!"
     for person in participants:
         if person not in passMe:
             winner = person
     
     print"Sold to " + winner + " for $" +  str(highestBid) + "!!"
+
+
+    if highestBid > float(forAuction.get(itemBeingAuct)):
+        print "The auction was a sucess!"
+
+    else:
+        print "The aucion was a failure..."
 
             
 
@@ -116,6 +143,25 @@ def Auction(itemBeingAuct):
 
 
 
-decision = raw_input("Would you like to add a new item to the auction?""**Just press Enter**")
-if len(decision) < 1:
-    auctionSetup()
+
+auctionSetup()
+
+print"The bidding for the "+itemBeingAuct+" is now over!"
+    for person in participants:
+        if person not in passMe:
+            winner = person
+    
+print"Sold to " + winner + " for $" +  str(highestBid) + "!!"
+
+
+if highestBid > float(forAuction.get(itemBeingAuct)):
+    print "The auction was a sucess!"
+
+else:
+    print "The aucion was a failure..."
+
+del forAuction[itemBeingAuct]
+
+if len(forAuction) > 0:
+    participantSetup()
+    
